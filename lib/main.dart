dd9413d06d225a0e33063ab518f843c6fe68e338
pqas1234-crypto <pqas1234@gmail.com>
2026-05-03 03:54:58 +0800

    Found the code! Uploading from backup_lib

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  theme: ThemeData.dark(),
+  home: Scaffold(
+    appBar: AppBar(title: Text('MegaRoute 中部戰區導航')),
+    body: Center(child: Text('全自動雲端編譯測試中...', style: TextStyle(fontSize: 20))),
+  ),
+));
